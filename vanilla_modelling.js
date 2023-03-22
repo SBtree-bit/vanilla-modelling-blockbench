1f65b5f9fa78ba63bba9b11f782610b089196e7b
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2023-03-22 07:38:03 +0100

    Update vanilla_modelling.js

--- vanilla_modelling.js
+++ vanilla_modelling.js
@@ -1,3 +1,21 @@
 (function() {
-
+  let activate_button;
+  Plugin.register('vanilla_modelling', {
+    title: "Vanilla Modelling",
+    author: "SBtree",
+    description: "This converts Blockbench models into datapacks that add your models to vanilla minecraft with no resource packs!",
+    icon: 'icon-player',
+    version: '0.0.1',
+    variant: 'both',
+    onload() {
+      activate_button = new Action('create_datapack', {
+        name: "Create Datapack",
+        description: "Put the model into a vanilla datapack",
+        icon: 'icon-player',
+        click: function() {
+          console.log("Generating datapack")
+        }
+      })
+    }
+  })
 })()
