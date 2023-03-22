de279b95567448ef1f28a299adb6000f5489b324
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2023-03-22 07:49:26 +0100

    Update vanilla_modelling.js

--- vanilla_modelling.js
+++ vanilla_modelling.js
@@ -1,21 +1,36 @@
 (function() {
-  let activate_button;
-  Plugin.register('vanilla_modelling', {
-    title: "Vanilla Modelling",
-    author: "SBtree",
-    description: "This converts Blockbench models into datapacks that add your models to vanilla minecraft with no resource packs!",
-    icon: 'icon-player',
-    version: '0.0.1',
-    variant: 'both',
-    onload() {
-      activate_button = new Action('create_datapack', {
-        name: "Create Datapack",
-        description: "Put the model into a vanilla datapack",
-        icon: 'icon-player',
-        click: function() {
-          console.log("Generating datapack")
+    var button;
+
+    Plugin.register('vanilla_modellling', {
+        title: 'Height Randomizer',
+        author: 'YourName',
+        description: 'This plugin can randomize the height of all selected cubes',
+        icon: 'bar_chart',
+        version: '0.0.1',
+        variant: 'both',
+        onload() {
+            button = new Action('randomize_height', {
+                name: 'Randomize Height',
+                description: 'Randomize the height of all selected elements',
+                icon: 'bar_chart',
+                click: function() {
+                    Undo.initEdit({elements: Cube.selected});
+                    Cube.selected.forEach(cube => {
+                        cube.to[1] = cube.from[0] + Math.floor(Math.random()*8);
+                    });
+                    Canvas.updateView({
+                        elements: Cube.selected,
+                        element_aspects: {geometry: true},
+                        selection: true
+                    });
+                    Undo.finishEdit('Randomize cube height');
+                }
+            });
+            MenuBar.addAction(button, 'filter');
+        },
+        onunload() {
+            button.delete();
         }
-      })
-    }
-  })
-})()
+    });
+
+})();
